babcc0e867fb864b0725a0cd32511229a410e404
rastislav <bolibruch2000@gmail.com>
2023-03-03 21:08:07 +0100

    added test view

--- frontend/routes.tsx
+++ frontend/routes.tsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter, IndexRouteObject, NonIndexRouteObject, useMatches } from 'react-router-dom';
 import {NewAppView} from "Frontend/views/newapp.js";
+import {Testing} from "Frontend/views/tryingMyOwn.js";
 
 const router = createBrowserRouter([
     { path: '/', element: <NewAppView /> },
+    { path: '/testing', element: <Testing />},
 ]);
 export default router;
---
+++ frontend/views/tryingMyOwn.tsx
@@ -0,0 +1,7 @@
+export function Testing(){
+    return(
+        <div>
+            <h1>test</h1>
+        </div>
+    )
+}
